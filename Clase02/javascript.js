ae51413fd1f33e14816fd65c2ddbb2c1c6a6fda5
Francary <francarymolinapaez@gmail.com>
2024-03-21 22:11:28 -0300

    02 Clase

---
+++ Clase02/javascript.js
@@ -0,0 +1,17 @@
+console.log("Hola Francary Molina")
+
+let nombre = "Francary"
+const apellido = "Molina"
+alert("Hola ingresa un numero")
+
+let num1 = Number(prompt("Ingresar primer numero:"))
+let num2 = Number(prompt("Ingresar segundo numero:"))
+
+const sumar = (num1,num2) =>{
+    alert(num1+num2)
+    document.write(num1+num2)
+
+}
+
+sumar(num1,num2)
+
