cfa50e1e970803412ce1d75d70056163b2a7fdc8
Francary <francarymolinapaez@gmail.com>
2024-04-14 08:44:30 -0300

    Mejoras de Clase 02

--- Clase02/javascript.js
+++ Clase02/javascript.js
@@ -1,7 +1,9 @@
-console.log("Hola Francary Molina")
-
+// Console Log Basico
 let nombre = "Francary"
 const apellido = "Molina"
+console.log(`Hola ${nombre} ${apellido}`)
+
+// Realizar Suma
 alert("Hola vamos a realizar una Suma")
 
 let num1 = Number(prompt("Ingresar primer numero:"))
@@ -9,9 +11,21 @@ let num2 = Number(prompt("Ingresar segundo numero:"))
 
 const sumar = (num1,num2) =>{
     alert(` La suma de los 2 numero es: ${num1 + num2}`)
-    document.write(num1+num2)
+    document.write(` La suma de los 2 numero es: ${num1 + num2}<br><br>`)
 
 }
 
 sumar(num1,num2)
 
+// Usar if Basico
+
+let edad = 18
+const mayorEdad = () =>{
+if (edad >= 18){
+    document.write(`Puede Ingresar es mayor de Edad`)
+}
+else{
+    document.write(`No puedes pasar solmante tienes ${edad} años`)
+}
+}
+mayorEdad()
